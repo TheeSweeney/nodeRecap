93a5cfdf512ba8508157faa40b2f487c25f706f9
Sweeney <brian.sweeney@ibm.com>
2018-11-12 16:55:01 -0500

    building a module files;

--- D2_FirstClassFunctions/Starter/app.js
+++ D2_FirstClassFunctions/Starter/app.js
@@ -1,1 +1,24 @@
 // Your Javascript Code Goes Here
+
+function greet(){
+    console.log('hellos');
+}
+
+greet();
+
+//functions are first class, ie are objects and can be used the same way as any other variable. In this example they can be passed as paramenters into functions;
+
+function logGreeting(fn){
+    fn();
+}
+
+logGreeting(greet);
+
+//function expression - an anonymous function assigned to a variable. still first class
+
+var greetMe = function() {
+    console.log('Hola')
+}
+
+greetMe();
+logGreeting(greetMe)
